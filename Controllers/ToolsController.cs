9cd3718b04ff8d29514d95c4d4cab53df36a5125
Jorge Aguado Merlet <aguado.jorge@principal.com>
2019-03-12 00:42:31 -0300

    add email and document endpoints

---
+++ Controllers/ToolsController.cs
@@ -0,0 +1,38 @@
+﻿using AdminInmuebles.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminInmuebles.Controllers
+{
+    [Route("v1/[controller]")]
+    public class ToolsController : BaseController
+    {
+        [HttpPost("mail")]
+        public async Task<IActionResult> SendEmail(Models.Email email)
+        {
+            var response = await Helpers.Email.Send(email.From, email.To, email.Subject, email.PlainTextContent, email.HtmlContent, email.AttachementName, email.AttachementContent);
+            var body = await response.Body.ReadAsStringAsync();
+            return new ContentResult() { Content=body, StatusCode = (int) response.StatusCode };
+        }
+
+        [HttpGet("document/{id}")]
+        [Produces(typeof(Models.Document))]
+        public IActionResult GetDocument(string id)
+        {
+            //TODO get document from db
+            return new OkObjectResult(new Models.Document());
+        }
+        [HttpPost("document")]
+        public IActionResult SaveDocument(Models.Document document)
+        {
+            //TODO save document
+            return new NoContentResult();
+        }
+    } 
+}
---
+++ Models/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminInmuebles.Models
+{
+    public class Email
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string PlainTextContent { get; set; }
+        public string HtmlContent { get; set; }
+        public string AttachementName { get; set; }
+        public string AttachementContent { get; set; }
+    }
+}
