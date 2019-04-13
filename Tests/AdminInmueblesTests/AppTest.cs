cf96f00ae77bd73b64a0b3a7f04cebd50998a7b1
Jorge <jorge@jamtech.cl>
2019-04-13 00:20:34 -0400

    add empty tests and upgrade to netcore 2.2

---
+++ Tests/AdminInmueblesTests/AppTest.cs
@@ -0,0 +1,15 @@
+using AdminInmuebles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdminInmueblesTests
+{
+    [TestClass]
+    public class AppTest
+    {
+        [TestMethod]
+        public void AlwaysTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
