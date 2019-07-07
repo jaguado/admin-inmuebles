7aeeb6584a1ba2ec444de6f5610791b1bcc0b5e0
Jorge <jorge@jamtech.cl>
2019-07-07 17:50:26 -0400

    add dummy properties for all users

--- template/src/app/shared/mockdata.ts
+++ template/src/app/shared/mockdata.ts
@@ -50,50 +50,52 @@ export const DefaultMenu: Menu[] = [
   }
 ];
 
+export const DefaultProperties: any[] = [
+  {
+    'id': 1,
+    'alias': 'Departamento',
+    'icon': '/assets/images/icons/apartment.svg',
+    'chartLabels': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+    'chartData': [
+      { data: [50000, 79000, 80000, 81000, 56000, 55000, 40000], label: 'Deuda' },
+      { data: [28000, 79000, 80000, 50000, 56000, 55000, 0], label: 'Pago' }
+    ]
+  },
+  {
+    'id': 2,
+    'alias': 'Casa',
+    'icon': '/assets/images/icons/small-house.svg',
+    'chartLabels': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+    'chartData': [
+      { data: [50000, 79000, 80000, 81000, 56000, 55000, 40000], label: 'Deuda' },
+      { data: [58000, 59000, 40000, 100000, 56000, 55000, 0], label: 'Pago' }
+    ]
+  },
+  {
+    'id': 3,
+    'alias': 'Minimarket',
+    'icon': '/assets/images/icons/shop-house.svg',
+    'chartLabels': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+    'chartData': [
+      { data: [200000, 79000, 80000, 81000, 56000, 55000, 400000], label: 'Deuda' },
+      { data: [280000, 79000, 800000, 50000, 56000, 55000, 500000], label: 'Pago' }
+    ]
+  }
+];
+
 export const DefaultCondos: Condo[] = [
   {
     'id': 1,
     'name': 'Condominio Principal',
     'menu': DefaultMenu,
     'enabled': true,
-    'properties': [
-      {
-        'id': 1,
-        'alias': 'Departamento',
-        'icon': '/assets/images/icons/apartment.svg',
-        'chartLabels': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
-        'chartData': [
-          { data: [50000, 79000, 80000, 81000, 56000, 55000, 40000], label: 'Deuda' },
-          { data: [28000, 79000, 80000, 50000, 56000, 55000, 0], label: 'Pago' }
-        ]
-      },
-      {
-        'id': 2,
-        'alias': 'Casa',
-        'icon': '/assets/images/icons/small-house.svg',
-        'chartLabels': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
-        'chartData': [
-          { data: [50000, 79000, 80000, 81000, 56000, 55000, 40000], label: 'Deuda' },
-          { data: [58000, 59000, 40000, 100000, 56000, 55000, 0], label: 'Pago' }
-        ]
-      },
-      {
-        'id': 3,
-        'alias': 'Minimarket',
-        'icon': '/assets/images/icons/shop-house.svg',
-        'chartLabels': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
-        'chartData': [
-          { data: [200000, 79000, 80000, 81000, 56000, 55000, 400000], label: 'Deuda' },
-          { data: [280000, 79000, 800000, 50000, 56000, 55000, 500000], label: 'Pago' }
-        ]
-      }
-    ]
+    'properties': DefaultProperties
   },
   {
     'id': 2,
     'name': 'Edificio Vacaciones',
     'menu': DefaultMenu,
     'enabled': false,
-    'properties': []
+    'properties': DefaultProperties
   }
 ];
