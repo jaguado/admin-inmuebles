22041cc99518e69eb22c56334c99a3d6d7de02c4
Jorge <jorge@jamtech.cl>
2019-07-10 02:15:34 -0400

    admin module working read only

--- template/src/app/admin/admin.component.ts
+++ template/src/app/admin/admin.component.ts
@@ -1,4 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { HttpClient } from '@angular/common/http';  // Import it up here
+import { AuthService } from '../auth.service';
+import { environment } from '../../environments/environment';
+
+export interface Table {
+  Position ?: number;
+  BD: string;
+  Nombre: string;
+  Esquema: string;
+}
+
+export interface Columns {
+  Position ?: number;
+  Nombre: string;
+  Tipo: string;
+  Largo: number;
+  Precision: number;
+}
+
+export interface DataColumn {
+  Name: string;
+}
+
+export enum View {
+  Tables = 1, Columns = 2, Data = 3
+}
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +33,105 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  dataSourceTables = null;
+  dataSourceColumns = null;
+  dataSourceData = null;
+  places: Array<any> = [];
+  currentView: View = null;
+  selectedTable: Table = null;
+  dataColumns: DataColumn[] = [];
+  dataDisplayedColumns: string[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.currentView = View.Tables;
+    this.loadTables();
   }
 
+  view(table: Table) {
+    console.log('view structure', table);
+    this.selectedTable = table;
+    this.currentView = View.Columns;
+    this.loadColumns();
+  }
+
+
+  data(table: Table) {
+    console.log('view data', table);
+    this.selectedTable = table;
+    this.currentView = View.Data;
+    this.loadData();
+  }
+
+  goBack(){
+    this.selectedTable = null;
+    this.currentView = View.Tables;
+  }
+
+  loadTables() {
+    this.http.get(environment.baseUrl + 'v1/GenericForms')
+    .toPromise()
+    .then((res: Table[]) => {
+      if (res) {
+        let int = 0;
+        res.forEach(row => {
+          row.Position = ++int;
+        });
+      }
+      console.log('loadTables', res);
+      this.dataSourceTables = new MatTableDataSource(res);
+    })
+    .catch(err => {
+      console.log('loadTables error', err);
+    })
+    .finally(() => {
+    });
+  }
+
+  loadColumns() {
+    this.http.get(environment.baseUrl + 'v1/GenericForms/' + this.selectedTable.Nombre)
+    .toPromise()
+    .then((res: Columns[]) => {
+      if (res) {
+        let int = 0;
+        res.forEach(row => {
+          row.Position = ++int;
+        });
+      }
+      console.log('loadColumns', res);
+      this.dataSourceColumns = new MatTableDataSource(res);
+    })
+    .catch(err => {
+      console.log('loadColumns error', err);
+    })
+    .finally(() => {
+    });
+  }
+
+  loadData() {
+    this.dataColumns = [];
+    this.dataDisplayedColumns = [];
+    this.http.get(environment.baseUrl + 'v1/GenericForms/' + this.selectedTable.Nombre + '/data')
+    .toPromise()
+    .then((res: any[]) => {
+      if (res) {
+        if (res.length > 0) {
+          const firstRow = res[0];
+          console.log('loadData', 'firstRow', firstRow);
+          Object.keys(firstRow).forEach(p => {
+            this.dataColumns.push({ Name: p});
+          });
+          this.dataDisplayedColumns = this.dataColumns.map(column => column.Name);
+        }
+        console.log('loadData', res);
+        this.dataSourceData = new MatTableDataSource(res);
+      }
+    })
+    .catch(err => {
+      console.log('loadData error', err);
+    })
+    .finally(() => {
+    });
+  }
 }
--- template/src/app/admin/admin.module.ts
+++ template/src/app/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { MatButtonModule, MatCardModule, MatIconModule, MatTableModule } from '@angular/material';
 
 @NgModule({
-    imports: [CommonModule, AdminRoutingModule],
+    imports: [CommonModule, AdminRoutingModule, MatButtonModule, MatCardModule, MatIconModule, MatTableModule],
     declarations: [AdminComponent]
 })
 export class AdminModule {}
